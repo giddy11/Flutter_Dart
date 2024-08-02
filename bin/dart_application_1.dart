ff825146c510b0e78281bb79641d5b1ed07acb15
giddy11 <edoghotugiddy@gmail.com>
2024-08-02 10:54:15 +0100

    first commit

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,10 @@
+import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
+
+void main(List<String> arguments) {
+  // dart_application_1.section1();
+  // dart_application_1.section2();
+  // dart_application_1.section3();
+  // dart_application_1.section4();
+  // dart_application_1.section5();
+  dart_application_1.section6();
+}
